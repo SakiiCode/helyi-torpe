6b8047b3a04dece4eb83a1b9c4c6b3df903854be
SakiiCode <sakiicode10@gmail.com>
2019-01-27 16:08:17 +0100

    normális tördelés képgenerálálskor

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ const sharp = require('sharp');
 const TextToSVG = require('text-to-svg');
 const textToSVG = TextToSVG.loadSync('Anonymous_Pro.ttf');
 
+
 //const s2i = require('svg2img');
 const { convert } = require('convert-svg-to-png');
+
+var wrap = require('word-wrap');
+
+var sizeOf = require('buffer-image-size');
+
+var Promise = require("bluebird");
+
 
 var port = process.env.PORT || 3000
 
 const attributes = {fill: 'black'};
-const options = {x: 0, y: 0, fontSize: 50, anchor: 'top', attributes: attributes};
- 
+const options = {x: 0, y: 0, fontSize: 48, anchor: 'top', attributes: attributes};
+
+  var splitChars = [ ' ', '-', '\t' ];
+  var letterWidthPixels = 28;
+  var letterHeightPx = 65;
 
 
 client.on('ready', () => {
@@ -36,7 +47,7 @@ client.on('message', msg => {
 			
 			text=msg.content.substring(6);
 					  
-					  
+			
 					  
 					  
 
@@ -44,21 +55,34 @@ client.on('message', msg => {
 			bigw=700;
 			
 			
-			svg = textToSVG.getSVG(text, options);
 
 			//SZÖVEG MÉRETEI
-			txtpadding=Math.max(100-text.length*5, 20);
-			//txtw=bigw-2*txtpadding;
-			txth=100;
+			txtpadding=20;//Math.max(100-text.length*5, 20);
+			txtwmax=bigw-2*txtpadding;
+			//txth=100;
 			//fontsize=50;
 			//console.log(txtpadding);
+			
+			
+			charsPerLines = Math.floor(txtwmax/letterWidthPixels);  
+			
+			//lines = wrap(text, {width: charsPerLines, indent:'', newline: "\n", trim: true});
+			lines = WordWrap(text, charsPerLines);
+			linesArr = lines.split(/\r\n|\r|\n/);
+			linesCount = linesArr.length;
+			
+			txth=letterHeightPx*linesCount;
+			
+			svgs = [];
+			for(i=0;i<linesCount;i++){
+				svgs.push(textToSVG.getSVG(linesArr[i], options));
+			}
 
-				
 			//BELSŐ KÉP MÉRETEI
 			imgpadding=20;
 			destw=bigw-2*imgpadding;
 			desth=480;
-			console.log("Dest:"+destw+","+desth);
+			
 			
 			//KÉP HELYE FELÜLRŐL
 			imgy=txth+2*txtpadding+imgpadding; 
@@ -66,12 +90,17 @@ client.on('message', msg => {
 			//NAGY KÉP MÉRETEI
 			bigh=txth+2*txtpadding+desth+2*imgpadding;
 			console.log("Big:"+bigw+","+bigh);
-
-					  
+			console.log("Textlines("+(txtwmax/letterWidthPixels)+"):"+charsPerLines+"*"+linesCount);
+			console.log("Txtpadding:"+txtpadding);
+			console.log("Imgpadding:"+imgpadding);
+			console.log("DestSize:"+destw+","+desth);	  
 			
 			
 			var request = require('request').defaults({ encoding: null });
 			request.get(url, function (err, res, body) {
+				if(err){
+					console.error("Download error:" + err);
+				}
 				sharp(body)
 					.resize({width:destw, height:desth, fit: 'inside'})
 					.toBuffer((err, data, info) => {
@@ -87,28 +116,48 @@ client.on('message', msg => {
 						}
 					})
 					.overlayWith(data,{
-							top:imgy,
+							top:imgy+((bigh-imgy)/2-info.height/2),
 							left:(bigw/2-info.width/2)
 						})
 					.png()
-					.toBuffer(async(err2, data2, info2) => {
+					.toBuffer((err2, data2, info2) => {
+						if(err2){
+							console.error("Image overlay error: " + err2);
+							return;
+						}
+						currentLine=0;
+						Promise.reduce(svgs, async function(total, svg) {
+							s2i = await convert(svg, {
+								puppeteer: {
+									args: ['--no-sandbox', '--disable-setuid-sandbox']
+								}
+							});
+							
+							dimensions = sizeOf(s2i);
+							console.log(dimensions.width, dimensions.height);
+							
+							return sharp(total)
+							.overlayWith(s2i,{
+								top:txtpadding+currentLine*letterHeightPx,
+								left:txtpadding
+							})
+							.png()
+							.toBuffer()
+							.then((data3) => { currentLine++; return data3; })
+							.catch(err3 => { console.error("Text overlay error: " + err3); });
 
-						s2i = await convert(svg, {
-							puppeteer: {
-								args: ['--no-sandbox', '--disable-setuid-sandbox']
-							}
+						}, data2).then(function(total) {
+							msg.channel.send(msg.author,{files:[total]});
+								
+								msg.delete().catch(err => {console.error(err.message);});
 						});
-
-						sharp(data2)
-						.overlayWith(s2i,{
-							top:txtpadding,
-							left:txtpadding
-						})
-						.png()
-						.toBuffer((err3, data3, info3) => {
-							msg.channel.send(msg.author,{files:[data3]});//{embed:new Discord.RichEmbed({image:data3})});
-							msg.delete();
-						})
+						
+						/*svgs.reduce(async (previousPromise, nextID) => {
+							await previousPromise;
+							
+							
+							
+						}, Promise.resolve());*/
 						
 						
 						
@@ -142,6 +191,59 @@ client.on('message', msg => {
   }
   
 });
+
+function WordWrap(str, width)
+{
+    splitChars = [ ' ', '-', '\t' ];
+newLine='\n';
+    words = str.split(' ');
+		//words= Explode(str, splitChars);
+    
+    curLineLength = 0;
+    strBuilder='';
+    //StringBuilder strBuilder = new StringBuilder();
+    for(i = 0; i < words.length; i += 1)
+    {
+    	debugger;
+        word = words[i]+" ";
+        // If adding the new word to the current line would be too long,
+        // then put it on a new line (and split it up if it's too long).
+        if (curLineLength + word.length > width)
+        {
+            // Only move down to a new line if we have text on the current line.
+            // Avoids situation where wrapped whitespace causes emptylines in text.
+            if (curLineLength > 0)
+            {
+            	
+                //strBuilder.Append(Environment.NewLine);
+                strBuilder += newLine;
+                curLineLength = 0;
+            }
+
+            // If the current word is too long to fit on a line even on it's own then
+            // split the word up.
+            while (word.length > width)
+            {
+            		
+                //strBuilder.Append(word.Substring(0, width - 1) + "-");
+                strBuilder += word.substr(0,width-1) + "-";
+                //word = word.Substring(width - 1);
+
+                word = word.substr(width - 1);
+
+                //strBuilder.Append(Environment.NewLine);
+                strBuilder += newLine;
+            }
+
+            // Remove leading whitespace from the word so the new line starts flush to the left.
+            word = word.trimStart();
+        }
+        strBuilder += word;
+        curLineLength += word.length;
+    }
+
+    return strBuilder;
+}
 
 client.login(process.env.TOKEN);
 
